ad5c7c6d21f93790abdad3dc54b7eb4d3917275b
Vitalij <almizvitalij@gmail.com>
2021-03-23 17:41:04 +0200

    Update AddContactForm.js

--- src/Components/AddContactForm/AddContactForm.js
+++ src/Components/AddContactForm/AddContactForm.js
@@ -1,94 +1,80 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputMask from 'react-input-mask';
 
 import contactsOperations from '../../Redux/contacts/contacts-operations';
 import contactsSelectors from '../../Redux/contacts/contacts-selectors';
-
-import s from './AddContactForm.module.css';
 import authActions from '../../Redux/auth/auth-actions';
 
-class AddContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+import s from './AddContactForm.module.css';
 
-  onChange = e => {
-    this.setState({
-      [e.currentTarget.name]: e.currentTarget.value,
-    });
-  };
+export default function AddContactForm() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [warningShown, setWarningShown] = useState(false);
+  const [textNotification, setTextNotification] = useState('');
 
-  onSubmitForm = e => {
+  const dispatch = useDispatch();
+  
+  const allContacts = useSelector(contactsSelectors.getAllContacts);
+
+  function onChangeName(e) {
+    setName(e.currentTarget.value);
+  }
+
+  function onChangeNumber(e) {
+    setNumber(e.currentTarget.value);
+  }
+
+  function onSubmitForm(e) {
     e.preventDefault();
-    const { name, number } = this.state;
+    const contactName = name;
 
     if (!name) {
-      this.props.showWarning('Enter contact name.');
+      showWarning('Enter contact name.');
     } else if (!number) {
-      this.props.showWarning('Enter contact number.');
-    } else if (
-      this.props.allContacts.some(({ name }) => name === this.state.name)
-    ) {
-      this.props.showWarning('This contact is already on the list.');
+      showWarning('Enter contact number.');
+    } else if (allContacts.some(({ name }) => name === contactName)) {
+      showWarning('This contact is already on the list.');
     } else {
-      this.props.onAddContact(this.state);
-      this.setState({ name: '', number: '' });
+      dispatch(contactsOperations.addContact({ name, number }));
+
+      setName('');
+      setNumber('');
     }
   };
 
-  render() {
-    const { number, name } = this.state;
+  return (
+    <>
+      <form className={s.form} onSubmit={onSubmitForm}>
+        <label className={s.label}>
+          Name
+          <input
+            className={s.formInput}
+            name="name"
+            onChange={onChangeName}
+            value={name}
+          ></input>
+        </label>
 
-    return (
-      <>
-        <form className={s.form} onSubmit={this.onSubmitForm}>
-          <label className={s.label}>
-            Name
-            <input
-              className={s.formInput}
-              name="name"
-              onChange={this.onChange}
-              value={name}
-            ></input>
-          </label>
+        <label className={s.label}>
+          Number
+          <InputMask
+            className={s.formInput}
+            type="tel"
+            name="number"
+            onChange={onChangeNumber}
+            value={number}
+            mask="+3\8(999) 999-99-99"
+            maskChar={null}
+          />
+        </label>
 
-          <label className={s.label}>
-            Number
-            <InputMask
-              className={s.formInput}
-              type="tel"
-              name="number"
-              onChange={this.onChange}
-              value={number}
-              mask="+3\8(999) 999-99-99"
-              maskChar={null}
-            />
-          </label>
-
-          <button className={s.btn} type="submit">
-            Add contact
-          </button>
-        </form>
-      </>
-    );
-  }
+        <button className={s.btn} type="submit">
+          Add contact
+        </button>
+      </form>
+    </>
+  );
 }
-
-AddContactForm.propTypes = {
-  allContacts: PropTypes.array,
-};
-
-const mapStateToProps = state => ({
-  allContacts: contactsSelectors.getAllContacts(state),
-});
-
-const mapDispatchToProps = {
-  onAddContact: contactsOperations.addContact,
-  showWarning: authActions.registerError,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AddContactForm);
